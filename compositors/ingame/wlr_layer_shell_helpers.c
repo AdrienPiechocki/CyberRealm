13852a7a19589b95082cfd50fa3a7c6f7fdd8a5a
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2026-08-17 10:01:00 +0200

    KO

--- compositors/ingame/wlr_layer_shell_helpers.c
+++
@@ -1,8 +0,0 @@
-#include "wlr_layer_shell_helpers.h"
-
-const char *waylandgodot_layer_surface_get_namespace(const struct wlr_layer_surface_v1 *surface) {
-	if (surface == NULL) {
-		return NULL;
-	}
-	return surface->namespace;
-}
