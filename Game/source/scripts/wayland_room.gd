12558bd692590e5acd9e0333b1923aed73d37b8d
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2026-08-18 16:59:49 +0200

    fixes

--- Game/source/scripts/wayland_room.gd
+++ Game/source/scripts/wayland_room.gd
@@ -556,7 +556,6 @@ func _process(delta: float) -> void:
 	# On inverse l'état du mode interaction à chaque fois que la touche est pressée
 	# (le clic molette sert au client en mode focus, pas au toggle du mode interaction).
 	if Input.is_action_just_pressed("interact_mode", true) and not focus.is_active():
-		layers.deactivate_layer_interact()
 		if interact_mode_active:
 			compositor.release_all_keys()
 		interact_mode_active = not interact_mode_active
