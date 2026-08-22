44b1ca58d68138207094feb86681595067a17e81
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2026-08-22 17:43:53 +0200

    Update windows_3d.gd

--- Game/source/scripts/windows_3d.gd
+++ Game/source/scripts/windows_3d.gd
@@ -548,12 +548,16 @@ func on_window_decorations_changed(id: int, server_side: bool) -> void:
 func on_window_unmapped(id: int) -> void:
 	if focused_window_id == id:
 		focused_window_id = -1
+	
 	window_textures.erase(id)
 	window_shared.erase(id)
 	_texture_versions.erase(id)
 	if quads.has(id):
 		var quad = quads[id]
 		if is_instance_valid(quad):
+			var occ := quad.get_node_or_null("Occluder") as OccluderInstance3D
+			occ.queue_free()
+			await get_tree().physics_frame
 			quad.queue_free()
 		quads.erase(id)
 	windows_state_changed.emit()
