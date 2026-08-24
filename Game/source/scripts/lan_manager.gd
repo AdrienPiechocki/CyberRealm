92c90f8552806e6b1acfcc4b4de2196c1f900b95
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2026-08-24 22:46:00 +0200

    Update lan_manager.gd

--- Game/source/scripts/lan_manager.gd
+++ Game/source/scripts/lan_manager.gd
@@ -621,7 +621,7 @@ func _add_drop_target(avatar: Node3D) -> void:
 	cap.radius = 0.4
 	cap.height = 1.8
 	cs.shape = cap
-	cs.position = avatar.position
+	cs.position = Vector3(0, 0.9, 0)
 	body.add_child(cs)
 	avatar.add_child(body)
 
