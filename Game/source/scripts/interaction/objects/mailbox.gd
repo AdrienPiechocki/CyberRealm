b2ff6057d311fc5538dd9e3544972e1ccd93c483
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2026-09-11 14:37:52 +0200

    Revert "Update mailbox.gd"
    This reverts commit 975433c434ef5ff97de6f046018d2b87ae5112af.

--- Game/source/scripts/interaction/objects/mailbox.gd
+++ Game/source/scripts/interaction/objects/mailbox.gd
@@ -11,9 +11,9 @@ var _panel: PanelContainer = null
 var _list: VBoxContainer = null
 
 func _ready() -> void:
-	_resolve_bridge()
+	if _bridge == null:
+		_bridge = get_tree().get_first_node_in_group("host_services")
 	_subscribe()
-	call_deferred(&"_subscribe")
 
 func set_bridge(b) -> void:
 	_bridge = b
